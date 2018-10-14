1c11355905f81cf331cbca33a75d61bdd3600fcd
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-14 14:34:21 -0400

    Edits

--- docs/index.js
+++ docs/index.js
@@ -27,38 +27,5 @@ let options = {
   'backdropColor': 'green'
 }
 let element = 'place';
-
-let data2 = [
-  45,
-  74,
-  95,
-  34,
-  65,
-  44,
-  85,
-  54
-];
-let options2 = {
-  'width': '400px',
-  'height': '220px',
-  'barColor': 'orange',
-  'y': 'Student Marks (%)',
-  'x': 'Students',
-  'dataLabel': [
-    'Eric',
-    'Zac',
-    'Melanie',
-    'Mel',
-    'Judy',
-    'Su',
-    'Sam',
-    'Alex'
-  ],
-  'backdropColor': 'yellow'
-}
-let element2 = 'second';
-
 let barChart = new BarChart(data, options, element);
 barChart.createBarGraph();
-// let otherbarChart = new BarChart(data2, options2, element2);
-// otherbarChart.createBarGraph();
